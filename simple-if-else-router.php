30abaa888d757f4e0c112e6b439b681d955c2b63
Milutin Gavrilovic <milutingavrilovic@yahoo.com>
2019-10-12 11:29:33 +0200

    Create simple-if-else-router.php

---
+++ simple-if-else-router.php
@@ -0,0 +1,19 @@
+<?php
+
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+
+require_once(ROOT_DIR .'include/header.php');
+require_once(ROOT_DIR .'include/meni.php');
+
+$link = $_GET['t'];
+if($link=="1") {
+require_once(ROOT_DIR .'pages/1.php');
+} elseif ($link=="2") {
+require_once(ROOT_DIR .'pages/2.php');
+}  elseif ($link=="3") {
+require_once(ROOT_DIR .'pages/3.php');
+}  else  {
+echo '<p>Jesam ja lepo rekao, medvedima pristup zabranjen</p>';
+}
+
+require_once(ROOT_DIR .'include/footer.php');
